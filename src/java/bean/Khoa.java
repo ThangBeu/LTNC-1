c9fc690636e97c36c8c7cd0efd1145a9778cc4a7
ThangBeu <61720607+ThangBeu@users.noreply.github.com>
2020-12-23 10:43:42 +0700

    Revert "Thang"
    This reverts commit 37808144cdb08d16aa2e0c3bb4b65f86f481d169.

--- src/java/bean/Khoa.java
+++ src/java/bean/Khoa.java
@@ -5,24 +5,15 @@
  */
 package bean;
 
-import java.util.Comparator;
-
 /**
  *
  * @author thai
  */
 public class Khoa {
-private String maKhoa;
+    private String maKhoa;
     private String tenKhoa;
     private String diaChi;
     private String dienThoai;
-    
-    public Khoa() {
-        
-    }
-
-    
-
 
     public String getMaKhoa() {
         return maKhoa;
@@ -55,25 +46,6 @@ private String maKhoa;
     public void setDienThoai(String dienThoai) {
         this.dienThoai = dienThoai;
     }
-        public Khoa(String maKhoa, String tenKhoa, String diaChi, String dienThoai) {
-        this.maKhoa = maKhoa;
-        this.tenKhoa = tenKhoa;
-        this.diaChi = diaChi;
-        this.dienThoai = dienThoai;
-    }
-        
-    public static Comparator<Khoa> tenKhoaCompare = new Comparator<Khoa>() {
-        @Override
-        public int compare(Khoa o1, Khoa o2) {
-            return o1.tenKhoa.compareTo(o2.tenKhoa);
-        }
-    };
     
-    public static Comparator<Khoa> maKhoaCompare = new Comparator<Khoa>() {
-        @Override
-        public int compare(Khoa o1, Khoa o2) {
-            return o1.maKhoa.compareTo(o2.maKhoa);
-        }
-    };
+    
 }
-
